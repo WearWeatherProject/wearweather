1254be4cc38317c4245f81ab2063dbadd5db1ca8
wn1331 <wn1331@gmail.com>
2023-03-21 15:47:52 +0900

    init commit&push

---
+++ src/main/java/org/project/wearweather/WearweatherApplication.java
@@ -0,0 +1,13 @@
+package org.project.wearweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/wearweather/WearweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.wearweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
