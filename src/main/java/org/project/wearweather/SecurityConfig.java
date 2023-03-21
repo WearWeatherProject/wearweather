a506050349a012b9b18282f07dd46c1edb1f325c
wn1331 <wn1331@gmail.com>
2023-03-21 16:51:11 +0900

    부트스트랩 적용완료

--- src/main/java/org/project/wearweather/SecurityConfig.java
+++ src/main/java/org/project/wearweather/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/user/availability/**").permitAll()
+                .antMatchers("/","/css/**","/fonts/**","/images/**","/js/**","/vendor/**", "/user/availability/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
