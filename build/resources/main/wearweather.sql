73f35b0cb4bff0d7451f6b51186217470ae07a05
hyunji <hyungi246@naver.com>
2023-03-21 18:01:42 +0900

    trash_files

--- build/resources/main/wearweather.sql
+++
@@ -1,9 +0,0 @@
---Root 계정에서 실행.
-create database wearweather default character set utf8;
-create user 'shopuser'@'localhost' identified by 'shopuser';
-grant all privileges on wearweather.* to 'shopuser'@'localhost';
-create user 'shopuser'@'%' identified by 'shopuser';
-grant all privileges on wearweather.* to 'shopuser'@'%';
-
---루트계정 비밀번호를 1234로 변경한다.
-ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1234';
