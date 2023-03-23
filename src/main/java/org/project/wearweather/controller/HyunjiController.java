a6dd5c5548effc2f1a4e3e98ee45e7f07910a2be
wn1331 <wn1331@gmail.com>
2023-03-23 16:01:05 +0900

    revert

--- src/main/java/org/project/wearweather/controller/HyunjiController.java
+++ src/main/java/org/project/wearweather/controller/HyunjiController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/member")
 public class HyunjiController {
-    // 로그인 폼
 
-    @GetMapping("/login")
-    public String login(){
-        return "user/login";
-    }
+    // 로그인 폼
+    @RequestMapping("/login")
+    public String login() {return "/user/login";}
+
+
 
 
 
