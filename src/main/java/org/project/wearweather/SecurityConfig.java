862340e4678f69c8d3980aebcab8f09570fc4677
hyunji <hyungi246@naver.com>
2023-03-29 17:18:46 +0900

    230329_시큐리티 로그인 기능 구현ing, 로그아웃 경로 생성

--- src/main/java/org/project/wearweather/SecurityConfig.java
+++ src/main/java/org/project/wearweather/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.project.wearweather;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,14 +13,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import static org.project.wearweather.entity.Role.ROLE_ADMIN;
 
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Configuration
-@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+@EnableGlobalMethodSecurity(securedEnabled = true) //  @Secured 어노테이션 활성화 (권한이 필요한 API에 @Secured 어노테이션 추가하여 권한 설정할 수 있다. 스프링 시큐리티가 로그인한 회원의 권한을 인식하도록 수정한다.)
+@Configuration // 서버가 기동될 때 설정해주겠다.
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.(스프링 security 지원을 가능하게 함)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC 로 등록해준다.
     @Bean
@@ -27,14 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+    // BCryptPasswordEncoder 는 Spring Security 에서 제공하는 비밀번호 암호화 객체이다.
+    // Service 에서 비밀번호를 암호화할 수 있도록 Bean 으로 등록한다.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    // js, css, image 설정은 보안 설정의 영향 밖에 있도록 만들어주는 설정
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 
@@ -49,14 +57,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/user/login")
-                .loginProcessingUrl("/loginPost") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다. (controller 에 /login 을 안넣어줘도 된다. 시큐리티가 대신 해줌)
+                .loginPage("/user/login") // 로그인 View 제공 (GET /user/login)
+                .loginProcessingUrl("/loginPost") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다. (controller 에 /login 을 안넣어줘도 된다. 시큐리티가 대신 해줌) // 로그인 처리 (POST /user/loginPost)
                 .usernameParameter("userId")
                 .passwordParameter("userPwd")
-                .defaultSuccessUrl("/") // 로그인 완료 되면 메인페이지로 가게 만든다.
+                .defaultSuccessUrl("/") // 로그인 처리 후 성공 시 URL
+                .failureUrl("/user/login?error") // 로그인 처리 후 실패 시 URL
                 .permitAll()
                 .and()
                 .logout()
+                .logoutUrl("/user/logout") // 로그아웃 요청 처리 URL
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .permitAll();
--- src/main/java/org/project/wearweather/controller/HyunjiController.java
+++ src/main/java/org/project/wearweather/controller/HyunjiController.java
@@ -13,12 +13,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HyunjiController {
 
     // 스프링시큐리티가 해당 주소를 낚아챔 - SecurityConfig 파일 생성 후 작동안함.
-    // 로그인 폼
+    // 로그인 폼 페이지
     @GetMapping("/login")
     public String login() {return "/user/login";}
 
+    // 로그인 버튼 누르면 실행되는 아이
+    // 로그인 성공시 -> "redirect:/"
+    //       실패시 -> "redirect:/user/login?error"
     @PostMapping("/loginPost")
     public void loginPOST(UserDTO dto) {}
+
+
+    // 로그아웃
+    /*
+    "GET /user/logout" 로 API 설계 했는데, "POST /user/logout" 으로 처리 필요
+    CSRF protection 이 기본적으로 enable 되어 있기 때문
+    CSRF protection 을 disable 하면 GET /user/logout 으로도 사용 가능
+    */
+    @GetMapping("/logout")
+    public String logout() {return "redirect:/user/login";}
 
 
     // 장바구니
